b5256f730213d37981c11959656abd1f747181ef
Tornike Dzidziguri <tornikedzidziguri2001@gmail.com>
2024-02-09 12:40:24 +0400

    first commit

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char fraza[] = "hello world";
+    char characterName[] = "Rene";
+    int characterAge = 35;
+    printf("%s\n",fraza);
+    printf("There once was a man named %s\n", characterName);
+    printf("He was %d years old.\n", characterAge);
+
+    characterAge = 23;
+    printf("He really liked name %s.\n",characterName);
+    printf("But did not like being %d\n",characterAge);
+
+    printf("my fav %s is %d","number",12);
+    return 0;
+}
